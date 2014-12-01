69922b3dc8b5a285e5ddafb6d13734fc7a7564f5
Masahiro Hayashi <hayashi.masahiro@gmail.com>
2014-12-01 13:00:57 +0900

    Add fragment

--- app/src/main/java/io/github/mhayashi/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/io/github/mhayashi/photogallery/PhotoGalleryActivity.java
@@ -1,65 +1,11 @@
 package io.github.mhayashi.photogallery;
 
-import android.app.Fragment;
-import android.app.ActionBar;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-
+import android.support.v4.app.Fragment;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_photo_gallery);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                                .add(R.id.container, new PlaceholderFragment())
-                                .commit();
-        }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_photo_gallery, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
-            return rootView;
-        }
+    public Fragment createFragment() {
+        return new PhotoGalleryFragment();
     }
 }
---
+++ app/src/main/java/io/github/mhayashi/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,29 @@
+package io.github.mhayashi.photogallery;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+/**
+ * Created by Masahiro Hayashi on 12/1/14.
+ */
+public class PhotoGalleryFragment extends Fragment {
+    GridView mGridView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mGridView = (GridView)v.findViewById(R.id.gridView);
+        return v;
+    }
+}
