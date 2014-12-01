789f7c359f868c247f641c063a71e73e361185c3
Masahiro Hayashi <hayashi.masahiro@gmail.com>
2014-12-01 13:13:05 +0900

    Fetch web content

--- app/src/main/java/io/github/mhayashi/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/io/github/mhayashi/photogallery/PhotoGalleryFragment.java
@@ -1,23 +1,30 @@
 package io.github.mhayashi.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import java.io.IOException;
+
 /**
  * Created by Masahiro Hayashi on 12/1/14.
  */
 public class PhotoGalleryFragment extends Fragment {
+    private static final String TAG = "PhotoGalleryFragment";
+
     GridView mGridView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Override
@@ -25,5 +32,18 @@ public class PhotoGalleryFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
         mGridView = (GridView)v.findViewById(R.id.gridView);
         return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetcher().getUrl("http://www.google.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
     }
 }
